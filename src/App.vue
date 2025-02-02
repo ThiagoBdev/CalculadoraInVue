9ff71ed73c4642558079b26b4e7dd5862798afaa
Thiago Batista <thiagobatista15390@gmail.com>
2025-02-02 15:03:18 -0300

    Primeiro commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+<div class="container">
+    <header>
+        <h1>Calculadora</h1>
+    </header>
+    <form>
+        <input type="number" id="numero1" placeholder="Escolha um numero:">
+        <select>
+            <option>Soma</option>
+            <option>Subtração</option>
+            <option>Multiplicação</option>
+            <option>Divisão</option>
+        </select>
+        <input type="number" id="numero2" placeholder="Escolha outro numero numero:">
+    </form>
+</div>
+</template>
+
+<style lang="scss">
+
+</style>
